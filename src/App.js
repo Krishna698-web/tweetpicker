734dfbd4fc61e390b57833562ec55a471584bb3d
Krishan <kmkrider1999@gmail.com>
2024-04-29 23:02:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState("");
+
+  const tweet_URL = "https://x.com/RadioGenoa/status/1784486537041785257";
+  const token =
+    "AAAAAAAAAAAAAAAAAAAAAAk9tgEAAAAAeeJ408b9ecL1IoAMCWzED5TAXnY%3DwlJkUFefYjera5E1N8jtSR0feluIPikOgODVJgvQzKQIIgjoWv";
+
+  const tweetId = tweet_URL.split("/").pop();
+
+  const fetch_tweets = async () => {
+    try {
+      const res = await fetch(
+        `https://api.twitter.com/2/tweets/${tweetId}?tweet.fields=author_id,created_at,public_metrics`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      console.log(await res.json());
+    } catch (error) {
+      console.error("Error fetch the tweet: ", error);
+    }
+  };
+
+  fetch_tweets();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Krishna starts here again...</h2>
       </header>
     </div>
   );
